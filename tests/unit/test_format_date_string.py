1fb4ba0537b95730a8ebc9f5095fed03b31c636c
KristinaRiabova <kristinaer304@gmail.com>
2023-10-09 21:52:39 +0300

    Unit test (format date string)(fixed)

--- tests/unit/test_format_date_string.py
+++ tests/unit/test_format_date_string.py
@@ -1,20 +1,18 @@
 import unittest
-from user_stats_api import format_date_string  # Import the function you want to test
+from user_stats_api import format_date_string
 
 class TestYourCode(unittest.TestCase):
 
     def test_format_date_string(self):
-        # Test a valid date string
+
         input_date = "2023-10-09T12:34"
         expected_output = "2023-10-09T12:34"
         self.assertEqual(format_date_string(input_date), expected_output)
 
-        # Test an invalid date string (just to show an example)
         input_date = "2023-10-09 12:34"
         with self.assertRaises(ValueError):
             format_date_string(input_date)
 
-    # You can add more unit tests for other functions here
 
 if __name__ == '__main__':
     unittest.main()
